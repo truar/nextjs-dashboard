99136cec503c4c85ff667ba91add04d607e81afd
truar <thibault.ruaro@gmail.com>
2025-05-04 18:12:51 +0200

    next tuto step 5

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { ReactNode } from 'react';
+
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
